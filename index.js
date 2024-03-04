54d3c1746b5a812ef2a5e5c33e581c1ec126b58d
Ahmad Tantawy <ahmadtantawy.dev@gmail.com>
2024-03-03 18:58:49 -0800

    Merge current progress with function implementations

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import {
   inputElement,
   taskListElement,
   getDeleteElements,
+  getListItemElements,
 } from './scripts/elements';
 
 // fetchData
@@ -29,22 +30,18 @@ const deleteTask = (index) => {
   tasks.splice(index, 1);
 
   saveToDB('tasks', tasks);
-  renderTaskList(tasks);
-  initTaskListeners();
+  initTaskList(tasks);
 };
-/* eslint-enable */
 
 // RenderTaskList
 const renderTaskList = (tasks) => {
   let taskList = '';
   tasks.forEach((item) => {
-    const checkedAttribute = item.isCompleted ? 'checked' : '';
-
     taskList += `
-    <li>
-        <label class="list-item">
-          <input class="checkbox" type="checkbox" ${checkedAttribute}/>
-          <span class="todo-text">${item.value}</span>
+    <li class="${item.isCompleted ? "List__isCompleted" : "List__isActive"}">
+          <label class="list-item">
+            <input class="checkbox" type="checkbox"/>
+            <span class="todo-text">${item.value}</span>
           </label>
           <span class="remove"></span>
     </li>`;
@@ -56,11 +53,17 @@ const renderTaskList = (tasks) => {
 // InitTaskListeners Function
 const initTaskListeners = () => {
   const deleteElements = getDeleteElements();
+  const listElements = getListItemElements();
 
   deleteElements.forEach((element, index) => {
     element.addEventListener('click', () => deleteTask(index));
   });
+
+  listElements.forEach((element, index) => {
+    element.addEventListener('click', (event) => toggleCompletdTask(event, index));
+  });
 };
+/* eslint-enable */
 
 // AddTask Function
 const addTask = (event) => {
@@ -77,28 +80,70 @@ const addTask = (event) => {
   const tasks = fetchData('tasks') || [];
   tasks.push(task);
   saveToDB('tasks', tasks);
-
-  renderTaskList(tasks);
-  initTaskListeners();
+  /* eslint-disable */
+  initTaskList(tasks);
 };
 
 // DarkTheme
-// Store the old and new image sources
-const oldSrc = 'images/icon-moon.svg';
-const newSrc = 'images/icon-sun.svg';
-let isDarkMode = false;
+const toggleDarkMode = () => {
+  // Store the old and new image sources
+  const oldSrc = 'images/icon-moon.svg';
+  const newSrc = 'images/icon-sun.svg';
+  let isDarkMode = false;
 
-appThemetoggleElement.addEventListener('click', () => {
-  isDarkMode = !isDarkMode;
-
-  // Toggle the 'App--isDark' class
-  bodyElement.classList.toggle('App--isDark');
-  // Update the image source based on the isDarkMode flag
-  if (isDarkMode) {
-    appThemetoggleElementImage.setAttribute('src', newSrc);
-  } else {
-    appThemetoggleElementImage.setAttribute('src', oldSrc);
+  // Check if dark mode flag exists in local storage
+  if (fetchData('darkModeFlag')) {
+    isDarkMode = true;
+    // Toggle the 'App--isDark' class
+    bodyElement.classList.add('App--isDark');
   }
-});
+
+  appThemetoggleElement.addEventListener('click', () => {
+    isDarkMode = !isDarkMode;
+
+    // Toggle the 'App--isDark' class
+    bodyElement.classList.toggle('App--isDark');
+    saveToDB('darkModeFlag', bodyElement?.classList.contains('App--isDark'));
+    // Update the image source based on the isDarkMode flag
+    if (isDarkMode) {
+      appThemetoggleElementImage.setAttribute('src', newSrc);
+    } else {
+      appThemetoggleElementImage.setAttribute('src', oldSrc);
+    }
+  });
+};
+
+// ToggleCompletedTask
+const toggleCompletdTask = (event, index) => {
+  const tasks = fetchData('tasks');
+
+  event.currentTarget.parentElement.classList.toggle('List__isCompleted');
+  tasks[index].isCompleted = !tasks[index].isCompleted;
+  saveToDB('tasks', tasks);
+}
+
+// RenderEmptyState Function
+const renderEmptyState = () => {
+  taskListElement.innerHTML = `
+  <p class="emptyState">
+  List is currently empty.</p>
+  `;
+};
 
 addTaskButton.addEventListener('click', addTask);
+/* eslint-disable */
+const initDataOnStartup = () => {
+  fetchData('darkModeFlag') ? toggleDarkMode() : toggleDarkMode(); 
+  initTaskList(fetchData('tasks'));
+};
+
+const initTaskList = (tasks) => {
+  if (tasks?.length) {  
+    renderTaskList(fetchData('tasks'))
+    initTaskListeners();
+  } else {
+    renderEmptyState();
+  }
+};
+
+initDataOnStartup();
--- scripts/elements.js
+++ scripts/elements.js
@@ -5,3 +5,4 @@ export const addTaskButton = document.querySelector('.app__search-bar__button');
 export const inputElement = document.querySelector('.app__search-bar__input');
 export const taskListElement = document.querySelector('.app__task-list__list');
 export const getDeleteElements = () => document.querySelectorAll('.remove');
+export const getListItemElements = () => document.querySelectorAll('.list-item');
